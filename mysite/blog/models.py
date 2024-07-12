ded3dc591b04df594d973fecd1c65dc01e973be4
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-07-12 21:35:25 +0300

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
