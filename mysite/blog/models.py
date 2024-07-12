2c59c7b42beb80b073b22c2f07e0699e13434aeb
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-07-12 21:33:57 +0300

    Creating the Post model && adding datetime fields

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
