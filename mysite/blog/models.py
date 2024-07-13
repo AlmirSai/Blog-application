e52239acf0428b61087dab71a9df2dbf638cedab
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-07-13 12:25:39 +0300

    Adding Published manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,13 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
 
 
 class Post(models.Model):
@@ -26,6 +33,8 @@ class Post(models.Model):
         default=Status.DRAFT,
 
     )
+    objects = models.Manager()
+    published = PublishManager()
 
     class Meta:
         ordering = ['-publish']
