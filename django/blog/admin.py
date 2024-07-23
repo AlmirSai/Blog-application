c377c6448a0ee76584d5c8b367d99ca6d8462d53
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-07-23 14:04:30 +0300

    Update admin.py and add type annotations

--- django/blog/admin.py
+++ django/blog/admin.py
@@ -1,3 +1,15 @@
+from typing import Literal
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display: list[str] = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter: list[str] = ['status', 'created', 'publish', 'author']
+    search_fields: list[str] = ['title', 'body']
+    prepopulated_fields: dict[str, tuple[Literal['title']]] = {'slug': ('title',)}
+    raw_id_fields: list[str] = ['author']
+    date_hierarchy: str = 'publish'
+    ordering: list[str] = ['status', 'publish']
