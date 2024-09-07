b36f55773551e7dd2c5f40cb52f4deeedfa66eaa
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-09-07 18:14:00 +0300

    Update codestyle

--- mySite/mySite/asgi.py
+++ mySite/mySite/asgi.py
@@ -1,11 +1,5 @@
-"""
-ASGI config for mySite project.
+"""App doesn't async architecture, but I don't edit this files"""
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 
 import os
 
