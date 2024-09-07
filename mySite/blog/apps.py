ad21fdad2f38763ea4c5b8869897e1efa86bd6a0
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-09-07 18:12:08 +0300

    Update codestyle

--- mySite/blog/apps.py
+++ mySite/blog/apps.py
@@ -1,6 +1,10 @@
+"""Config file for app 'blog'"""
+
+
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """Config class"""
     default_auto_field = "django.db.models.BigAutoField"
     name = "blog"
