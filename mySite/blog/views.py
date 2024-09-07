ce7588d83e392dc272270718264b21f42787ac4b
Almir <almirmacos@gmail.com>
2024-09-07 16:52:43 +0300

    Add template for detail view of list's and post's

--- mySite/blog/views.py
+++ mySite/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
