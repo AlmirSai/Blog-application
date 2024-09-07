8e7baaf17cce95c28f3b88297fd9cf86ad6f3284
AlmirSai <167138187+AlmirSai@users.noreply.github.com>
2024-09-07 18:11:50 +0300

    Update codestyle

--- mySite/blog/admin.py
+++ mySite/blog/admin.py
@@ -1,9 +1,13 @@
+"""File for acsess control for admins"""
+
+
 from django.contrib import admin
 from .models import Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """Class for controlling postings"""
     list_display = ['title', 'slug', 'author', 'publish', 'status']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
